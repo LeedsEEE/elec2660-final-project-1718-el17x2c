243236089d303066f2c3f6cf854c20ce058a19ed
Sean_CXY <sean_cxy@Sean-CXYdeMacBook-Pro.local>
2017-12-01 22:26:03 +0000

    I tried to use XYPieChart which taken from internet to draw pie chart,  but failed.

--- Project/ViewController.h
+++ Project/ViewController.h
@@ -5,9 +5,13 @@
 //  Created by Sean_CXY on 2017/11/29.
 //  Copyright © 2017年 University of Leeds. All rights reserved.
 //
-
+// take from https://codeplacer.wordpress.com/2015/01/22/how-to-draw-piechart-in-objective-c-using-xypiechart-library/
 #import <UIKit/UIKit.h>
+#import "XYPieChart.h"
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController <XYPieChartDelegate, XYPieChartDataSource>
+
+@property (strong,nonatomic) IBOutlet XYPieChart *pieChart;
+
 
 @end
--- Project/ViewController.m
+++ Project/ViewController.m
@@ -5,7 +5,7 @@
 //  Created by Sean_CXY on 2017/11/29.
 //  Copyright © 2017年 University of Leeds. All rights reserved.
 //
-
+// take fromhttps://codeplacer.wordpress.com/2015/01/22/how-to-draw-piechart-in-objective-c-using-xypiechart-library/
 #import "ViewController.h"
 
 @interface ViewController ()
@@ -17,6 +17,22 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    float viewWidth = self.pieChart.bounds.size.width / 2;
+    float viewHeight = self.pieChart.bounds.size.height / 2;
+    [self.pieChart setDelegate:self];
+    [self.pieChart setDataSource:self];
+    [self.pieChart setStartPieAngle:M_PI_2];
+    [self.pieChart setAnimationSpeed:1.5];
+    [self.pieChart setLabelColor:[UIColor whiteColor]];
+    [self.pieChart setLabelShadowColor:[UIColor blackColor]];
+    [self.pieChart setShowPercentage:YES];
+    [self.pieChart setPieBackgroundColor:[UIColor whiteColor]];
+    
+    //To make the chart at the center of view
+    [self.pieChart setPieCenter:CGPointMake(self.pieChart.bounds.origin.x + viewWidth, self.pieChart.bounds.origin.y + viewHeight)];
+    
+    //Method to display the pie chart with values.
+    [self.pieChart reloadData];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -33,5 +49,39 @@
     // Pass the selected object to the new view controller.
 }
 */
+
+- (NSUInteger)numberOfSlicesInPieChart:(XYPieChart *)pieChart
+{
+    return 2;
+}
+//Specify the Value for each sector
+- (CGFloat)pieChart:(XYPieChart *)pieChart valueForSliceAtIndex:(NSUInteger)index
+{
+    CGFloat value = 0.0;
+    if(index % 2 == 0)
+    {
+        value = 25;
+    }
+    else
+    {
+        value = 75;
+    }
+    return value;
+}
+
+//Specify color for each sector
+- (UIColor *)pieChart:(XYPieChart *)pieChart colorForSliceAtIndex:(NSUInteger)index
+{
+    UIColor *color;
+    if(index%2 == 0)
+    {
+        color = [UIColor whiteColor];
+    }
+    else
+    {
+        color = [UIColor blueColor];
+    }
+    return color;
+}
 
 @end
